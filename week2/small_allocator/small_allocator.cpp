76a0b6a85b5457e949ed4a4ad902119caf43e9c3
Roman Sofin <roma.sofin@gmail.com>
2019-11-28 08:55:45 +0300

    Keep working on SmallAllocator

--- week2/small_allocator/small_allocator.cpp
+++ week2/small_allocator/small_allocator.cpp
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <utility>
+#include <cstring>
 
 using namespace std;
 /* Следующий вариант и ему подобные считаются читерством
@@ -22,8 +23,9 @@ public:
 class SmallAllocator {
 private:
         char Memory[1048576];
-		static const size_t controlDataLen = sizeof(size_t);
-        using ChunkDescr = pair<size_t, decltype(controlDataLen)>;	// индекс начала блока в Memory и его длина
+		using ChunkLengthType = size_t;
+		static const size_t ctrlDataLen = sizeof(ChunkLengthType);
+        using ChunkDescr = pair<size_t, ChunkLengthType>;	// индекс начала блока в Memory и его длина
 		list<ChunkDescr> freeChunks;	// вектор свободных непрерывных блоков памяти
 public:
 		SmallAllocator() : freeChunks() {
@@ -39,13 +41,13 @@ public:
         	// ищём первый свободдный блок памяти достаточного размера
         	//	для этого обходим freeChunks, проверяя длину каждого
         	// возвращаем найденный блок
-			size_t occupiedChunkLen = Size + controlDataLen;
+			size_t occupiedChunkLen = Size + ctrlDataLen;
 			//for(const ChunkDescr& cd : freeChunks) {
 			for(list<ChunkDescr>::iterator it = freeChunks.begin(); it != freeChunks.end(); it++) {	// it++ or ++it ? ? ? ? ?
 				if (it->second >= occupiedChunkLen) {
 					unsigned int * p = (unsigned int *) &Memory[it->first];
 					*p = Size;
-					void * res = (void *) &Memory[it->first + sizeof(Size)];
+					void * res = (void *) &Memory[it->first + ctrlDataLen];
 					// проверить случай, когда найденный свободный блок "идеально" подходит по длине под размер запрашиваемого блока
 					// в этом случае не нужно создавать новый ChunkDescr
 					if (it->second > occupiedChunkLen) {
@@ -68,11 +70,27 @@ public:
 			}
 			cout << "Alloc(): returning NULL" << endl;
         	return NULL;
-        };
+        }
         
-		void *ReAlloc(void *Pointer, unsigned int Size) {return NULL;};
+		void *ReAlloc(void *Pointer, unsigned int Size) {
+			cout << "ReAlloc(): reallocate " << Size << " byte(s)" << endl;
+			void * res = Alloc(Size);
+			memcpy(res, Pointer,  static_cast<size_t>(*(static_cast<char*>(Pointer)-ctrlDataLen)));
+			Free(Pointer);
+			cout << "ReAlloc(): returning res" << endl;
+			return res;
+		}
         
-		void Free(void *Pointer) {return;};
+		void Free(void *Pointer) {
+			size_t memIndex = (static_cast<char*>(Pointer) - ctrlDataLen) - &Memory[0];
+			for(auto it = freeChunks.begin(); it != freeChunks.end(); it++) {
+				if(memIndex = it->first) {
+					freeChunks.erase(it);
+				}
+			}
+			
+			return;
+		}
 };
 
 int main(int argc, char **argv) {

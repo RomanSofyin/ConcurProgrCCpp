dc0ea8426eb0253960fd40dd434aed5bdb1ce9f9
Roman Sofyin <roman.sofyin@emc.com>
2019-12-18 08:11:42 +0300

    Keep doing week3 first practice lesson

--- week3/server.c
+++ week3/server.c
@@ -3,7 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
+#include <unistd.h>         // close()
 
 int main (int argc, char **argv) {
     int MasterSocket = socket(

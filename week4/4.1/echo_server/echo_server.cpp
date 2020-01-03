c668d8598ad1d9bf348517d2434926cf19208596
Roman Sofyin <roman.sofyin@emc.com>
2020-01-03 17:56:55 +0300

    Create 4.1 echo_server skeleton

---
+++ week4/4.1/echo_server/echo_server.cpp
@@ -0,0 +1,25 @@
+#include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define UNIX_SOCK_PATH "/tmp/echo.sock"
+
+int main(int argc, char **argv) {
+    
+    return 0;
+}
+
+
+
+
+
+
+
+

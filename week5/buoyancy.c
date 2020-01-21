d8d22e9c93aff5105342f3f73373e2989b730983
Roman Sofin <roma.sofin@gmail.com>
2020-01-21 09:22:13 +0300

    5.2.11 initial code

---
+++ week5/buoyancy.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>  // open
+#include <unistd.h> // write
+#include <signal.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        printf("fork() failed\n");
+    } else if (pid == 0) {
+        /* child code goes here */
+        signal(SIGINT, SIG_IGN);
+        signal(SIGTERM, SIG_IGN);
+        printf("Child process - goes to pause()\n");
+        pause();
+    } else {
+        /* parent code goes here */
+        int fd = open( "./pid", O_WRONLY|O_CREAT, 0);
+        write(fd, &pid, sizeof(pid));
+        printf("Parent process - completes\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
